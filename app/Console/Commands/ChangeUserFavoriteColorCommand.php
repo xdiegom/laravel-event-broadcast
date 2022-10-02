2d69b3031ac0849f092beb80972d0415966ef44c
Diego Melgar <diego.melgar.sv@gmail.com>
2022-10-02 13:24:33 -0600

    ✨ add user favorite color column and assign it with command

---
+++ app/Console/Commands/ChangeUserFavoriteColorCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\UserFavoriteColorChanged;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
+
+class ChangeUserFavoriteColorCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:change-color';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Change user favorite color';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        try {
+            /** @var \App\Models\User */
+            $user = User::first();
+
+            $colors = [
+                'Red',
+                'Blue',
+                'Purple',
+                'Yellow',
+                'Green'
+            ];
+
+            $user->update([
+                'favorite_color' => Arr::random($colors)
+            ]);
+
+            return Command::SUCCESS;
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            throw $th;
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $user = User::first();
-    $colors = [
-        'Red',
-        'Blue',
-        'Purple',
-        'Yellow',
-        'Green'
-    ];
-
-    $user->favorite_color = Arr::random($colors);
 
     return view('welcome', compact('user'));
 });
