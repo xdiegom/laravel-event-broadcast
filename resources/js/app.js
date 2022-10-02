392f86d8e2a0ec2dc9c1d9ec9db502955bb49339
Diego Melgar <diego.melgar.sv@gmail.com>
2022-10-02 15:29:51 -0600

    📦 add laravel-echo, pusher and listen to event

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+import Echo from "laravel-echo";
+import "./bootstrap";
+
+let echo = new Echo({
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    wsHost: import.meta.env.VITE_PUSHER_HOST ??`ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
+    enabledTransports: ["ws", "wss"],
+});
+
+echo.channel("favorite-color").listen("UserFavoriteColorChanged", (event) => {
+    const color = event.user.favorite_color.toLowerCase();
+
+    document.getElementById("favorite_color").innerHTML = event.user.favorite_color;
+    document.getElementById("favorite_color").className = 'font-bold ' + 'text-' + color + '-500'
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Broadcast Events - Laravel</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
 </head>
 
 <body>
     <div class="min-h-screen flex justify-center items-center font-thin">
         <p>{{ $user->name }}'s favorite color is
-            <span class="font-bold text-{{ strtolower($user->favorite_color) }}-500">{{ $user->favorite_color }}</span>
+            <span id="favorite_color" class="font-bold text-{{ strtolower($user->favorite_color) }}-500">{{ $user->favorite_color }}</span>
         </p>
     </div>
 </body>
